bcfe605f14832da3889146f124b486df3ee60055
AngelF9 <112910128+AngelF9@users.noreply.github.com>
2023-05-23 12:50:09 -0700

    Create question3.cpp

---
+++ question3.cpp
@@ -0,0 +1,15 @@
+// greet.cpp
+// Name(s):
+// TODO: Angel Fuentes
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hi, what's your name?" << endl;
+  string name; 
+  cin >> name;
+  
+  cout << "Hello, " << name << "!" << endl;
+  
+  return 0;
+}
