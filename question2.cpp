b05221e0f4a8eececb486054a456a5798f078965
AngelF9 <112910128+AngelF9@users.noreply.github.com>
2023-05-23 12:48:47 -0700

    Create question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,14 @@
+// echo.cpp
+// Name: Angel Fuentes
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num_burgers;
+  cout << "Hello, how many burgers would you like?" << endl;
+  cin >> num_burgers;
+  
+  cout << "You would like " << num_burgers << " burgers, correct?" << endl;
+
+  return 0;
+}
